6b35acd7fde5e847939f4b40dcc18cbb29fd6628
Dafeta <ejedafetaemmanuella@gmail.com>
2026-07-21 14:01:03 +0100

    add error message

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
 
 const ErrorMessage = ({message}) => {
     return (
-      <div className="alert alert-error my-4">
-        <span className="bg-red-500 ">{message}</span>
+      <div className="alert alert-error bg-red-500 p-2 my-4">
+        <span className="text-white">{message}</span>
       </div>
     )
   }
